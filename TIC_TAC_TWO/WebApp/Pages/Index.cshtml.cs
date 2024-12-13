2c8be3aa6e731502a9057f9e13adc2735bd16796
eriklihhats <eriklihhats@gmail.com>
2024-12-13 15:31:45 +0200

    Web UI and moves

--- TIC_TAC_TWO/WebApp/Pages/Index.cshtml.cs
+++ TIC_TAC_TWO/WebApp/Pages/Index.cshtml.cs
@@ -1,3 +1,4 @@
+using System.Security.Cryptography.X509Certificates;
 using DAL;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
@@ -6,30 +7,30 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 namespace WebApp.Pages;
 
 public class IndexModel : PageModel
-
 {
     private readonly ILogger<IndexModel> _logger;
     private readonly IConfigRepository _configRepository;
-    
+
     public IndexModel(ILogger<IndexModel> logger, IConfigRepository configRepository)
     {
         _logger = logger;
         _configRepository = configRepository;
     }
+
+    public SelectList? ConfigSelectList { get; set; }
     
-    public SelectList ConfigSelectList { get; set; } = default!;
-
-    [BindProperty] 
+    [BindProperty]
     public int ConfigId { get; set; }
-
     public IActionResult OnGet()
     {
         var selectListData = _configRepository.GetConfigurationNames()
-            .Select(name => new {id = name, value = name})
+            .Select(config => new
+            {
+                Id = config.Id, 
+                Name = config.Name
+            })
             .ToList();
-        
-        ConfigSelectList = new SelectList(selectListData, "id", "value");
-        
+        ConfigSelectList = new SelectList(selectListData, "Id", "Name");
         return Page();
     }
 
@@ -38,12 +39,15 @@ public class IndexModel : PageModel
         if (!ModelState.IsValid)
         {
             var selectListData = _configRepository.GetConfigurationNames()
-                .Select(name => new {id = name, value = name})
+                .Select(config => new
+                {
+                    Id = config.Id, 
+                    Name = config.Name
+                })
                 .ToList();
-        
-            ConfigSelectList = new SelectList(selectListData, "id", "value");
+            ConfigSelectList = new SelectList(selectListData, "Id", "Name");
+            return Page();
         }
-        
-        return RedirectToPage("./PlayGame", new { ConfigId = ConfigId });
+        return RedirectToPage("./PlayGame", new {ConfigId = this.ConfigId});
     }
 }
--- TIC_TAC_TWO/WebApp/Pages/PlayGame.cshtml.cs
+++ TIC_TAC_TWO/WebApp/Pages/PlayGame.cshtml.cs
@@ -10,24 +10,65 @@ public class PlayGame : PageModel
     private readonly IConfigRepository _configRepository;
     private readonly IGameRepository _gameRepository;
 
-    public PlayGame(IGameRepository gameRepository, IConfigRepository configRepository)
+    public PlayGame(IConfigRepository configRepository, IGameRepository gameRepository)
     {
-        _gameRepository = gameRepository;
         _configRepository = configRepository;
+        _gameRepository = gameRepository;
     }
 
-    [BindProperty(SupportsGet = true)] 
-    public int GameId { get; set; } = default!;
+    [BindProperty(SupportsGet = true)] public int GameId { get; set; } = default!;
 
-    [BindProperty(SupportsGet = true)] 
-    public EGamePiece NextMoveBy { get; set; } = default!;
-    
-    [BindProperty(SupportsGet = true)] 
-    public int ConfigId { get; set; }
+    [BindProperty(SupportsGet = true)] public int ConfigId { get; set; } = default!;
 
-    public TicTacTwoBrain TicTacToeBrain { get; set; } = default!;
+    [BindProperty] public int Y { get; set; } = default!;
 
-    public void OnGet(int? x, int? y)
+    [BindProperty] public int X { get; set; } = default!;
+
+    public TicTacTwoBrain TicTacTwoBrain { get; set; } = default!;
+    public string Message { get; set; } = string.Empty;
+
+    public IActionResult OnGet()
     {
+        if (GameId > 0)
+        {
+            var dbGame = _gameRepository.LoadGame(GameId);
+            TicTacTwoBrain = new TicTacTwoBrain(dbGame);
+            return Page();
+        }
+        else if (ConfigId > 0)
+        {
+            var dbConfig = _configRepository.GetConfigurationById(ConfigId);
+            TicTacTwoBrain = new TicTacTwoBrain(dbConfig, EGamePiece.X);
+            var gameState = TicTacTwoBrain.GetGameState();
+            _gameRepository.SaveGame(gameState, TicTacTwoBrain.GetGameConfigName());
+            GameId = (int)gameState.GameId!;
+
+            return RedirectToPage("./PlayGame", new { GameId = GameId });
+        }
+        else
+        {
+            TempData["Error"] = "No GameId or ConfigId provided.";
+            return RedirectToPage("./Index");
+        }
+    }
+
+    public IActionResult OnPost()
+    {
+        var dbGame = _gameRepository.LoadGame(GameId);
+        TicTacTwoBrain = new TicTacTwoBrain(dbGame);
+
+        bool moveResult = TicTacTwoBrain.MakeAMove(X, Y);
+        
+        if (moveResult)
+        {
+            _gameRepository.SaveGame(TicTacTwoBrain.GetGameState(), TicTacTwoBrain.GetGameConfigName());
+            return RedirectToPage("./PlayGame", new { GameId = GameId });
+        }
+        else
+        {
+            Message = "Invalid move.Please try again.";
+        }
+
+        return Page();
     }
 }
